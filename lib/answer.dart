37f946e323b0d346ac0de547f1d028278e896555
ChrAP <christopherapstarwars3@gmail.com>
2020-08-29 17:03:41 -0500

    Finished App

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function pressHandler;
+  final String answerText;
   
-  Answer(this.pressHandler);
+  Answer(this.pressHandler, this.answerText);
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: RaisedButton(
         color: Colors.brown,
-        child: Text('Ans 1'),
+        child: Text(this.answerText),
         onPressed: pressHandler,
         textColor: Colors.white,
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -18,19 +18,53 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
-  var questions = [
-    'Tell me your favorite color...',
-    'Tell me your favorite animal...'
+  final _questions = const [
+    {
+      'questionText': 'Tell me your favorite color...',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Blue', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ]
+    },
+    {
+      'questionText': 'Tell me your favorite animal...',
+      'answers': [
+        {'text': 'Cat', 'score': 4},
+        {'text': 'Bear', 'score': 3},
+        {'text': 'Dog', 'score': 1},
+        {'text': 'Lion', 'score': 8},
+        {'text': 'Snake', 'score': 10}
+      ]
+    },
+    {
+      'questionText': 'What\'s the thing you desire the most?',
+      'answers': [
+        {'text': 'Money', 'score': 10},
+        {'text': 'Power', 'score': 7},
+        {'text': 'Peace', 'score': 2},
+        {'text': 'Love', 'score': 1},
+        {'text': 'Eternity', 'score': 5}
+      ]
+    },
   ];
 
-  void ansQuestion() {
+  var _totalScore = 0;
+
+  void _resetQuiz() {
     setState(() {
-      if (_questionIndex < questions.length - 1)
-        _questionIndex++;
-      else
-        _questionIndex = 0;
+      _questionIndex = 0;
+      _totalScore = 0;
     });
-    print(_questionIndex);
+  }
+
+  void _ansQuestion(int score) {
+    _totalScore += score;
+
+    setState(() {
+      if (_questionIndex < _questions.length) _questionIndex++;
+    });
   }
 
   @override
@@ -41,16 +75,13 @@ class _MyAppState extends State<MyApp> {
           backgroundColor: Color(0xFF550000),
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex],
-            ),
-            Answer(ansQuestion),
-            Answer(ansQuestion),
-            Answer(ansQuestion),
-          ],
-        ),
+        body: (_questionIndex < _questions.length)
+            ? Quiz(
+                ansQuestion: _ansQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
